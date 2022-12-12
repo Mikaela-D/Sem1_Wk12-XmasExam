4a9d580af6e432e5048591f2274a1b540c6306ab
Mikaela <G00401392@atu.ie>
2022-12-12 09:19:11 +0000

    Added some code to Ship class

--- src/main/java/ie/atu/exam/Ship.java
+++ src/main/java/ie/atu/exam/Ship.java
@@ -1,4 +1,34 @@
 package ie.atu.exam;
 
 public class Ship {
+    private String shipName;
+    private String year;
+    private Integer minCrew;
+
+    public Ship(){
+    }
+
+    public String getShipName() {
+        return shipName;
+    }
+
+    public void setShipName(String shipName) {
+        this.shipName = shipName;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public Integer getMinCrew() {
+        return minCrew;
+    }
+
+    public void setMinCrew(Integer minCrew) {
+        this.minCrew = minCrew;
+    }
 }
